991979454b3645c51f3a0aac14ef8e6265414c6e
Регина <rmashkovtseva@gmail.com>
2023-02-16 15:06:44 +0300

    Доработка метода

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+	CheckingAccount checkingAccount = new CheckingAccount("Расчетный счет ", 0);
+    CreditAccount creditAccount = new CreditAccount("Кредитный счет", 200);
+    SavingsAccount savingsAccount = new SavingsAccount("Сберегательный счет", 3000);
+
+    savingsAccount.translation(savingsAccount,checkingAccount,scanner);
+
     }
 }
--- src/com/company/Score.java
+++ src/com/company/Score.java
@@ -13,17 +13,20 @@ public abstract class Score {
    }
 
     public void translation(Score score1, Score score2, Scanner scan){
-        System.out.println("Для перевода со счета " + score1.name + " на счет " + score2.name + " введите сумму");
-        int sum = scan.nextInt();
+        System.out.println("Для перевода с " + score1.name + " на " + score2.name + " введите сумму");
+        int sum;
         while(true){
-            if(sum <= score1.amount){
+           sum = scan.nextInt();
+            if(sum > score1.amount){
+                System.out.println("На счете " + score1.name + " нет суммы введенной вами, попробуйте еще раз");
+            }else{
                 score2.amount = score2.amount + sum;
                 score1.amount = score1.amount - sum;
-            }else{
-                System.out.println("На счете " + score1.name + " нет сумы введенной вами, попробуйте еще раз");
-
+                break;
             }
         }
+        System.out.println( score1.name + " " + score1.amount + " pуб.");
+        System.out.println( score2.name + " " + score2.amount + " pуб.");
 
 
     }
